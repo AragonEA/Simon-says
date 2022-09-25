fd622286f90322a80999d5cede995109bc11b381
Aragon Emanuel <aragon3m4@gmail.com>
2022-09-25 17:12:42 -0300

    Finish computer turn

--- js/main.js
+++ js/main.js
@@ -8,11 +8,36 @@ $playButton.onclick = startGame;
 
 function startRound() {
   changeTitle('Follow the sequence');
+  startComputerTurn();
+
+function startComputerTurn() {
+  let $square = getRandomSquare();
+  computerSequense.push($square);
+  computerSequense.forEach(function ($square, i) {
+    let delayMS = (i + 1) * 1000;
+    setTimeout(function () {
+      highlightSquare($square);
+    }, delayMS);
+  });
 }
 
 function changeTitle(title) {
   document.querySelector('#title').textContent = title;
 }
+
+function getRandomSquare() {
+  const $square = document.querySelectorAll('.square');
+  return $square[Math.floor(Math.random() * $square.length)];
+}
+
+
+function highlightSquare($square) {
+  $square.style.opacity = 1.5;
+  setTimeout(function () {
+    $square.style.opacity = 0.5;
+  }, 500);
+}
+
 function startGame() {
   resetVariables();
   startRound();
