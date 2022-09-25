75be793992f4aed33d6d3e26bbec59a7eda1f369
Aragon Emanuel <aragon3m4@gmail.com>
2022-09-25 16:22:22 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let computerSequense = [];
+let userSequence = [];
+let userScore = 0;
+let userBestScore = 0;
+let $playButton = document.querySelector('button[type=button]');
+
+$playButton.onclick = startGame;
+
+function startGame() {
+}
