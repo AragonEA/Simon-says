59addad2d553bf1349adce4e2b74d94333cdf331
Aragon Emanuel <aragon3m4@gmail.com>
2022-09-25 16:28:02 -0300

    Add startRound function

--- js/main.js
+++ js/main.js
@@ -6,8 +6,18 @@ let $playButton = document.querySelector('button[type=button]');
 
 $playButton.onclick = startGame;
 
+function startRound() {
+  changeTitle('Follow the sequence');
+}
+
+function changeTitle(title) {
+  document.querySelector('#title').textContent = title;
+}
 function startGame() {
   resetVariables();
+  startRound();
+}
+
 function resetVariables() {
   computerSequense = [];
   userSequence = [];
